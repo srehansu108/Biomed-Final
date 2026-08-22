17ea0210af9b8435b30b8e221c5d1f7350e69f99
SetuSouls03 <setusouls@gmail.com>
2026-08-22 17:12:15 +0530

    Second COmmit

--- Frontend/Frontend/src/api/axiosConfig.js
+++ Frontend/Frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Use import.meta.env instead of process.env in Vite
-const API_URL = 'http://localhost:8000/api/v1';
+const API_URL = 'https://biomed-final.onrender.com/api/v1';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
